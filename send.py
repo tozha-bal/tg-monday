cb4a8210af7f588eda0cc8ae4133abd9b52e4293
tozha-bal <sazho.tv@gmail.com>
2026-03-15 22:46:38 +0800

    Update send.py

--- send.py
+++ send.py
@@ -1,32 +1,16 @@
-import asyncio, os, logging
+import asyncio, os
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-import pytz
-
-logging.basicConfig(level=logging.INFO)
 
 API_ID   = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SESSION  = os.environ["SESSION"]
-GROUP    = os.environ["GROUP"]
+GROUP    = int(os.environ["GROUP"])
 MESSAGE  = os.environ["MESSAGE"]
 
-tz = pytz.timezone("Europe/Moscow")
-client = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
-
-async def send_message():
-    logging.info("Отправляю...")
-    await client.send_message(GROUP, MESSAGE)
-    logging.info("Отправлено!")
-
 async def main():
-    await client.start()
-    logging.info("Авторизован. Жду понедельника...")
-    scheduler = AsyncIOScheduler(timezone=tz)
-    scheduler.add_job(send_message, "cron", day_of_week="mon", hour=9, minute=0)
-    scheduler.start()
-    await asyncio.Event().wait()
+    async with TelegramClient(StringSession(SESSION), API_ID, API_HASH) as client:
+        await client.send_message(GROUP, MESSAGE)
+        print("Отправлено!")
 
 asyncio.run(main())
-
